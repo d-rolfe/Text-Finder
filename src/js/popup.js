346f9748449bce6b6a522c033e73c1e18a7925fb
Darren Rolfe <drolfe55@gmail.com>
2018-01-20 23:14:07 -0500

    changed directory structure

---
+++ src/js/popup.js
@@ -0,0 +1,40 @@
+function findText() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var request = {action: "findText"};
+    request.textToFind = document.getElementById("textToFind1").value;
+    chrome.tabs.sendMessage(tabs[0].id, request, function(response) {
+      console.log("got response from content script");
+      console.log(response);
+    });
+  });
+}
+document.addEventListener('DOMContentLoaded', () => {
+  
+  document.getElementById("findBtn").addEventListener("click", function() {
+    console.log("Find Button Clicked!");
+    findText();
+  });
+
+});
+
+// uuhhh 2
+//setup before functions
+  /*var typingTimer;                //timer identifier
+  var doneTypingInterval = 5000;  //time in ms, 5 second for example
+  var $input = $('#textToFind1');
+
+  //on keyup, start the countdown
+  $input.on('keyup', function () {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
+
+  //on keydown, clear the countdown 
+  $input.on('keydown', function () {
+    clearTimeout(typingTimer);
+  });
+
+  //user is "finished typing," do something
+  function doneTyping () {
+    console.log("firing!");
+  }*/
